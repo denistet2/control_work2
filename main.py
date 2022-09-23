40ba75fa97f090a9aeb09af73719234f8b1f83a5
denistet2 <76530306+denistet2@users.noreply.github.com>
2022-09-24 00:35:56 +0300

    control

--- main.py
+++ main.py
@@ -2,15 +2,29 @@
 # Требуется создать словарь, который в качестве ключей будет принимать данные числа (т. е. ключи будут типом int),
 # а в качестве значений – количество этих чисел в имеющейся последовательности.
 
-str1 = 1, 30, 55, 43, 98
-dic = dict.fromkeys(str1,len(str1))
+str1 = "124835737504846385650"
+print(len(str1))
+
+dic = {}
+for i in str1:
+    a =(str1.count(i))
+    d = (dict.fromkeys(i,a))
+    dic.update(d)
+for i in str1:
+    a = (str1.count(i))
+    d = (dict.fromkeys(i,a))
+    print(d)
 print(dic)
+
+
+
+
 
 # 2) Даны два списка чисел, которые могут содержать до 10000 чисел каждый.
 # Выведите все числа, которые входят как в первый, так и во второй список в порядке возрастания.
-_list1 = 1, 2, 600
-_list2 = 5, 67, 400
-_sort = sorted(_list1+_list2)
+_list1 = set([1, 2, 55, 89, 600])
+_list2 = set([5, 1, 400, 55])
+_sort = _list1 & _list2
 print(_sort)
 
 # 3) Имеется файл file.txt с текстом. Напишите программу,
@@ -19,7 +33,15 @@ print(_sort)
 # число строк.
 import os
 with open ('file.txt', 'r') as file:
-    data = file.read().replace(" ", "")
-    _count = len(data)
+
+    a = file.read().replace(" ","")
+
+    _count = 0
+    _num = 0
+    for i in a:
+        if i.isalpha():
+         _count +=1;
+        if i.isdigit():
+            _num +=1
     quantity = len(open('file.txt').readlines())
-    print(_count, quantity)
+    print(_count,_num, quantity)
